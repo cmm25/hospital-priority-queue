71b0b5a63a9a4e59dae3939adabd11112b0003ff
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-16 08:58:43 +0300

    buttons peek is empty and length added

--- main.py
+++ main.py
@@ -295,7 +295,7 @@ while running:
     draw_text("Remove Patient", remove_button_rect, color=BLACK)
     draw_text("Length", length_button_rect, color=BLACK)
     draw_text("Is Empty", is_empty_button_rect, color=BLACK)
-    draw_text("Peek", peek_button_rect, color=BLACK)
+    draw_text("Top", peek_button_rect, color=BLACK)
 
     # Draw priority queue
     draw_priority_queue()
